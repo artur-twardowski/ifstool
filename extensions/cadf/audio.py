df260460daf1c922f74f8dd3848ed3521e2aa354
artur-twardowski <artur.twardowski@gmail.com>
2022-03-13 13:52:30 +0100

    Added detection of repeated ID3v2 regions and zero-paddings

--- extensions/cadf/audio.py
+++ extensions/cadf/audio.py
@@ -8,6 +8,14 @@ class Extension_cadf_audio(Extension):
     READ_CHUNK_SIZE = 16384
     UNIQUE_FILES_POLICIES = ["drop", "ungroup", "group"]
 
+    ID3v2_HEADER_LENGTH = 10
+    ID3v2_MAGIC = b'ID3'
+    ID3v2_HEADER_REGION_SZ_BEGIN = 6
+    ID3v2_HEADER_REGION_SZ_END = 6 + 4
+    MP3_SYNC_WORD_SIZE = 2
+    MP3_SYNC_WORD_BITMASK = 0xFFF0
+    MP3_SYNC_WORD_CONTENT = 0xFFF0
+
     def __init__(self):
         self._unique_files_policy = "ungroup"
 
@@ -38,44 +46,101 @@ class Extension_cadf_audio(Extension):
         return False
 
     def _get_audio_region_mp3(self, filename):
+        """
+        Determines the offset and length of audio stream in MP3 file, skipping
+        ID3 tag block, ID3v2 tag regions and zero paddings.
+        """
+
+        # TODO: more in-depth analysis of mp3 audio stream to identify and
+        # exclude ID3v2 blocks appearing between MP3 frames. Unlikely scenario,
+        # but theoretically possible
         offset = 0
         length = 0
+        structure = []
         with open(filename, "rb") as f:
+            # save the length of file
             f.seek(0, io.SEEK_END)
             length = f.tell()
             f.seek(0, io.SEEK_SET)
-            # Check if ID3v2 region is present, and skip it to reach audio data
-            # ID3 header consists of 10 bytes: mmmvvxllll
-            # where:
-            #   * mmm = "ID3"
-            #   * vv - version
-            #   * x - flags
-            #   * llll - length
-            id3_magic = f.read(3)
-            if id3_magic == b'ID3':
-                id3_header = f.read(7)
-                offset += 10
-                id3_region_length = 0
-                for byte in range(3, 7):
-                    # synch-safe format - only 7 bits are used
-                    id3_region_length = (id3_region_length << 7) | id3_header[byte]
-                offset += id3_region_length
+            # There might be more than one ID3v2 regions in the file, so we need
+            # to traverse them all
+            last_loop_offset = -1
+            while offset < length:
+                if offset == last_loop_offset:
+                    raise Exception("BUG: Loop stuck at offset %d" % offset)
+                last_loop_offset = offset
+                
 
-                length -= offset
+                # Check if ID3v2 region is present, and skip it to reach audio data
+                # ID3 header consists of 10 bytes: mmmvvxllll
+                # where:
+                #   * mmm = "ID3"
+                #   * vv - version
+                #   * x - flags
+                #   * llll - length
+                id3_header = f.read(self.ID3v2_HEADER_LENGTH)
+
+                if id3_header[0:len(self.ID3v2_MAGIC)] == self.ID3v2_MAGIC:
+                    # read remaining bytes of ID3v2 header
+
+                    id3_region_length = 0
+                    for byte in range(self.ID3v2_HEADER_REGION_SZ_BEGIN, self.ID3v2_HEADER_REGION_SZ_END):
+                        # synch-safe format - only 7 bits are used
+                        id3_region_length = (id3_region_length << 7) | id3_header[byte]
+
+                    structure.append( (offset, id3_region_length, "ID3v2") )
+                    offset += id3_region_length + self.ID3v2_HEADER_LENGTH
+
+                    length -= offset
+                else:
+                    # unread the header
+                    f.seek(-self.ID3v2_HEADER_LENGTH, io.SEEK_CUR)
+
+                # Check if MP3 sync word is present.
+                # We expect to encounter 0xFFFB or 0xFFFA (in case there is an error correction bit set)
+                f.seek(offset, io.SEEK_SET)
+                mp3_frame_hdr_part = f.read(self.MP3_SYNC_WORD_SIZE)
+                sync_word = (mp3_frame_hdr_part[0] << 8) | (mp3_frame_hdr_part[1])
+
+                # If we read something else, it might be another ID3v2 region
+                if sync_word & self.MP3_SYNC_WORD_BITMASK == self.MP3_SYNC_WORD_CONTENT:
+                    # MP3 data encountered, audio stream begins here
+                    break
+                else:
+                    if mp3_frame_hdr_part == self.ID3v2_MAGIC[0:2]:
+                        # another ID3v2 region found, this is okay
+                        # rewind back and repeat
+                        f.seek(-self.MP3_SYNC_WORD_SIZE, io.SEEK_CUR)
+
+                    elif sync_word == 0:
+                        # zero-padding; rewind it all
+                        initial_offset = offset
+                        offset += 2
+                        while True:
+                            b = f.read(1)
+                            if b != b'\0':
+                                f.seek(-1, io.SEEK_CUR)
+                                break
+                            else:
+                                offset += 1
+                        structure.append( (initial_offset, offset-initial_offset, "Zero-padding") )
+                    else:
+                        structure.append( (offset, 0, "Unknown content starting with %04x" % sync_word))
+                        print("Unexpected data found in %s, determination of audio content might be inaccurate" % filename)
+                        break
 
             # Check if ID3 (v1) region is present
             f.seek(-128, io.SEEK_END)
             id3_magic = f.read(3)
             if id3_magic == b'TAG':
                 length -= 128
-
-        return offset, length
+        return offset, length, structure
 
 
     
     def after_file_added(self, entry):
         if entry.current_name.lower().endswith("mp3"):
-            offset, length = self._get_audio_region_mp3(entry.current_name)
+            offset, length, structure = self._get_audio_region_mp3(entry.current_name)
 
         h = hashlib.sha224()
         with open(entry.current_name, "rb") as f:
